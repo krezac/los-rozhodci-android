cab0e99e2b9e8001adf593aaa7e869b64953e189
Kamil Rezac <krezac@gmail.com>
2018-09-25 11:21:29 +0200

    Initial commit

---
+++ app/src/main/java/net/krezac/losrozhodciandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package net.krezac.losrozhodciandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
